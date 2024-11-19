ba5facdc771ad426a960469470792713c2c45d85
EngineerMoore <alexiashalise@gmail.com>
2024-11-19 07:56:59 -0600

    API boiler plate added

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require(`express`)
+const app = express()
+const PORT = 3000
+
+// Express.js logging middleware
+app.use(require("morgan")("dev"))
+
+app.get(`/`, (req, res, next) => {
+  res.send(`Welcome to the jukebox-mini API`)
+})
+
+
+app.use((req, res, next) => {
+  next({ stauts: 404, message: `Endpoint does not exist` })
+})
+
+app.use((e, req, res, next) => {
+  console.log(e);
+  res.status(e.status ?? 500).json(e.message `Something went wrong`)
+})
+
+app.listen(PORT, () => {
+  console.log(`You are now listening on port ${PORT}`)
+})
